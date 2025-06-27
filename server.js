31716b314c88e16e82d6ebf0c5ae34e23cfce3de
Rikiyaaa <122695941+Rikiyaaa@users.noreply.github.com>
2025-06-28 00:53:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/test', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`🚀 Server started on port ${PORT}`);
 });
